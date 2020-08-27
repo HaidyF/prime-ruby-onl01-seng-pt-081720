b169cc311a510824a44cdc514ca132393ceae458
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-27 14:51:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
-require prime.rb
-def prime? (i)
-    (1..10).to_array
-    return false if n < 2
-    (2..n/2).none?{|i| n % i == 0}
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
   end
 end
